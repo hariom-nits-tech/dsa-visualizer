4c00e7b97ea7b71b5159bc4517a858bf0050ae96
Hari Om Kumar <hariomnits@gmail.com>
2026-08-14 00:01:22 +0530

    Day 20: Added dark mode with theme presistance

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let comparisonCount = 0;
+let swapCount = 0;
+
 function generateArray(){
     
     let size=document.getElementById("size").value;
@@ -25,6 +28,7 @@ function generateArray(){
 }
 
 async function bubbleSort(){
+    let startTime = performance.now();
     document.getElementById("status").innerText="Sorting...";
     document.getElementById("generate").disabled =true;
     document.getElementById("sort").disabled = true;
@@ -32,6 +36,11 @@ async function bubbleSort(){
     document.getElementById("speed").disabled=true;
 
     let bars = document.querySelectorAll(".bar");
+    comparisonCount = 0;
+    swapCount = 0;
+
+    document.getElementById("comparisons").innerText = comparisonCount;
+    document.getElementById("swaps").innerText = swapCount;
     for(let i = 0; i<bars.length; i++){
         for(let j=0; j<bars.length-i-1; j++){
             let height1 = parseInt(bars[j].style.height);
@@ -41,6 +50,9 @@ async function bubbleSort(){
      let speed = document.getElementById("speed").value;
      await new Promise(resolve => setTimeout(resolve, speed));
 
+     comparisonCount++;
+     document.getElementById("comparisons").innerText = comparisonCount;
+
      if(height1 > height2){
         bars[j].style.height = `${height2}px`;
         bars[j+1].style.height = `${height1}px`;
@@ -48,6 +60,8 @@ async function bubbleSort(){
         let temp = bars[j].innerText;
     bars[j].innerText = bars[j+1].innerText;
     bars[j+1].innerText = temp;
+    swapCount++;
+    document.getElementById("swaps").innerText = swapCount;
     
     }
     bars[j].style.backgroundColor ="blue";
@@ -66,10 +80,14 @@ async function bubbleSort(){
     document.getElementById("status").innerText="Completed";
     document.getElementById("size").disabled=false;
     document.getElementById("speed").disabled=false;
+    let endTime = performance.now();
+    document.getElementById("time").innerText =
+    (endTime - startTime).toFixed(2) + " ms";
 }
 
 
 async function selectionSort(){
+    let startTime = performance.now();
     document.getElementById("status").innerText="Sorting...";
     document.getElementById("generate").disabled = true;
     document.getElementById("sort").disabled = true;
@@ -77,6 +95,11 @@ async function selectionSort(){
     document.getElementById("speed").disabled=true;
 
     let bars=document.querySelectorAll(".bar");
+    comparisonCount = 0;
+    swapCount = 0;
+
+    document.getElementById("comparisons").innerText = comparisonCount;
+    document.getElementById("swaps").innerText = swapCount;   
     for(let i = 0; i<bars.length; i++){
         let minIndex = i;
         for( let j= i+1; j<bars.length; j++){
@@ -86,6 +109,9 @@ async function selectionSort(){
             bars[j].style.backgroundColor = "red";
             let speed = document.getElementById("speed").value;
             await new Promise(resolve => setTimeout(resolve,speed));
+
+            comparisonCount++;
+            document.getElementById("comparisons").innerText = comparisonCount;    
 
             if(height2 < height1){
                 minIndex = j;
@@ -108,6 +134,8 @@ async function selectionSort(){
                   bars[i].innerText = bars[minIndex].innerText;
                   bars[minIndex].innerText = tempText;
                 }
+                swapCount++;
+                document.getElementById("swaps").innerText = swapCount;  
           bars[i].style.backgroundColor = "green";
     }
 
@@ -116,9 +144,13 @@ async function selectionSort(){
     document.getElementById("status").innerText="Completed";
     document.getElementById("size").disabled=false;
     document.getElementById("speed").disabled=false;
+    let endTime = performance.now();
+    document.getElementById("time").innerText =
+    (endTime - startTime).toFixed(2) + " ms";
 }
 
 async function insertionSort(){
+    let startTime = performance.now();
     document.getElementById("status").innerText="Sorting...";
     document.getElementById("generate").disabled = true;
     document.getElementById("sort").disabled = true;
@@ -126,6 +158,11 @@ async function insertionSort(){
     document.getElementById("speed").disabled=true;
 
     let bars = document.querySelectorAll(".bar");
+    comparisonCount = 0;
+    swapCount = 0;
+
+    document.getElementById("comparisons").innerText = comparisonCount;
+    document.getElementById("swaps").innerText = swapCount;
     for(let i = 1; i <bars.length; i++){
         let keyHeight = bars[i].style.height;
         let keyText = bars[i].innerText;
@@ -135,8 +172,15 @@ async function insertionSort(){
              parseInt(bars[j].style.height) >
              parseInt(keyHeight)
         ){
+            comparisonCount++;
+            document.getElementById("comparisons").innerText = comparisonCount;
             bars[j+1].style.height = bars[j].style.height;
             bars[j+1].innerText = bars[j].innerText;
+
+            swapCount++;
+            document.getElementById("swaps").innerText = swapCount;
+
+
             j-- ;
         }
         bars[j+1].style.height = keyHeight;
@@ -164,13 +208,23 @@ async function insertionSort(){
     document.getElementById("status").innerText="Completed";
     document.getElementById("size").disabled=false;
     document.getElementById("speed").disabled=false;
+    let endTime = performance.now();
+    document.getElementById("time").innerText =
+    (endTime - startTime).toFixed(2) + " ms";
 }
 async function mergeSort(){
+    let startTime = performance.now();
     document.getElementById("status").innerText="Sorting...";
     document.getElementById("generate").disabled = true;
     document.getElementById("sort").disabled = true;
     document.getElementById("size").disabled=true;
     document.getElementById("speed").disabled=true;
+
+    comparisonCount = 0;
+    swapCount = 0;
+
+    document.getElementById("comparisons").innerText = comparisonCount;
+    document.getElementById("swaps").innerText = swapCount;
 
     let bars = document.querySelectorAll(".bar");
     await merge(bars,0,bars.length-1);
@@ -184,6 +238,9 @@ async function mergeSort(){
     bars.forEach(bar=>{
         bar.style.backgroundColor="green"
     });
+    let endTime = performance.now();
+    document.getElementById("time").innerText =
+    (endTime - startTime).toFixed(2) + " ms"
 }
 async function merge(bars,left,right){
     if(left>=right){
@@ -193,6 +250,7 @@ async function merge(bars,left,right){
     await merge(bars, left,mid);
     await merge(bars, mid+1, right);
     await mergeArrays(bars,left, mid, right);
+    
 }
 async function mergeArrays(bars, left,mid,right){
     let leftArray =[];
@@ -208,6 +266,8 @@ async function mergeArrays(bars, left,mid,right){
     let j = 0;
     let k = left;
   while(i<leftArray.length && j< rightArray.length){
+    comparisonCount++;
+    document.getElementById("comparisons").innerText = comparisonCount;
     if(leftArray[i]<= rightArray[j]){
         bars[k].style.height =`${leftArray[i]}px`;
         bars[k].innerText =leftArray[i];
@@ -217,33 +277,41 @@ async function mergeArrays(bars, left,mid,right){
     }
     else{ bars[k].style.height=`${rightArray[j]}px`;
     bars[k].innerText = rightArray[j];
+    swapCount++;
+    document.getElementById("swaps").innerText = swapCount; 
     j++;
     k++;
    }
   }
   while(i<leftArray.length){
+    comparisonCount++;
+    document.getElementById("comparisons").innerText = comparisonCount;
      bars[k].style.height=`${leftArray[i]}px`;
      bars[k].innerText = leftArray[i];
+     swapCount++;
+     document.getElementById("swaps").innerText = swapCount;
      i++;
      k++;
   }
   while(j<rightArray.length){
+    comparisonCount++;
+    document.getElementById("comparisons").innerText = comparisonCount;
     bars[k].style.height=`${rightArray[j]}px`;
     bars[k].innerText=rightArray[j];
+    swapCount++;
+    document.getElementById("swaps").innerText = swapCount;
     j++;
     k++;
   }
   let speed = document.getElementById("speed").value;
   await new Promise(resolve=>setTimeout(resolve,speed));
 
-  document.getElementById("generate").disabled = false;
-  document.getElementById("sort").disabled=false;
-  document.getElementById("status").innerText="Completed";
-  document.getElementById("size").disabled=false;
-  document.getElementById("speed").disabled=false;
+  
+  
 }
 
 async function quickSort(){
+    let startTime = performance.now();
     document.getElementById("status").innerText="Sorting...";
     document.getElementById("generate").disabled =true;
     document.getElementById("sort").disabled = true;
@@ -251,6 +319,11 @@ async function quickSort(){
     document.getElementById("speed").disabled=true;
 
     let bars = document.querySelectorAll(".bar");
+    comparisonCount = 0;
+    swapCount = 0;
+
+    document.getElementById("comparisons").innerText = comparisonCount;
+    document.getElementById("swaps").innerText = swapCount;
     await quick(bars,0,bars.length-1);
     bars.forEach(bar=>{
         bar.style.backgroundColor ="green";
@@ -260,6 +333,9 @@ async function quickSort(){
     document.getElementById("status").innerText="Completed";
     document.getElementById("size").disabled=false;
     document.getElementById("speed").disabled=false;
+    let endTime = performance.now();
+    document.getElementById("time").innerText =
+    (endTime - startTime).toFixed(2) + " ms";
 
 }
 
@@ -276,6 +352,8 @@ async function partition(bars, low, high) {
     let i = low - 1;
 
     for (let j = low; j < high; j++) {
+        comparisonCount++;
+        document.getElementById("comparisons").innerText = comparisonCount;
 
         if (parseInt(bars[j].style.height) < pivot) {
             i++;
@@ -287,6 +365,8 @@ async function partition(bars, low, high) {
             let tempText = bars[i].innerText;
             bars[i].innerText = bars[j].innerText;
             bars[j].innerText = tempText;
+            swapCount++;
+            document.getElementById("swaps").innerText = swapCount;
         }
 
         let speed = document.getElementById("speed").value;
@@ -307,8 +387,11 @@ async function partition(bars, low, high) {
     let tempText = bars[i + 1].innerText;
     bars[i + 1].innerText = bars[high].innerText;
     bars[high].innerText = tempText;
+    swapCount++;
+    document.getElementById("swaps").innerText = swapCount;
 
     return i + 1;
+    
 }
 
 function updateAlgorithmInfo(){
@@ -354,6 +437,24 @@ function updateAlgorithmInfo(){
         document.getElementById("space").innerText="O(log n)";
         document.getElementById("description").innerText= "Quick Sort Selects a pivot and partitions the array around it";
      }
+    else if (algorithm == "linear") {
+    document.getElementById("algoName").innerText = "Linear Search";
+    document.getElementById("best").innerText = "O(1)";
+    document.getElementById("average").innerText = "O(n)";
+    document.getElementById("worst").innerText = "O(n)";
+    document.getElementById("space").innerText = "O(1)";
+    document.getElementById("description").innerText =
+        "Linear Search checks each element one by one until the target is found.";
+   }
+   else if (algorithm == "binary") {
+    document.getElementById("algoName").innerText = "Binary Search";
+    document.getElementById("best").innerText = "O(1)";
+    document.getElementById("average").innerText = "O(log n)";
+    document.getElementById("worst").innerText = "O(log n)";
+    document.getElementById("space").innerText = "O(1)";
+    document.getElementById("description").innerText =
+        "Binary Search repeatedly divides a sorted array into halves to find the target.";
+   }
 
 }
 
@@ -378,3 +479,74 @@ function startSorting(){
       
 }
 updateAlgorithmInfo();
+
+function startSearch(){
+    let value =parseInt(document.getElementById("searchValue").value);
+    if(isNaN(value)){
+        alert("Please enter a number.");
+        return;
+    }
+    linearSearch(value);
+}
+async function linearSearch(target) {
+    document.getElementById("generate").disabled = true;
+    document.getElementById("sort").disabled = true;
+    document.getElementById("search").disabled = true;
+    document.getElementById("size").disabled = true;
+    document.getElementById("speed").disabled = true;
+    let bars = document.querySelectorAll(".bar");
+
+    document.getElementById("status").innerText = "Searching...";
+
+    for (let i = 0; i < bars.length; i++) {
+
+        bars[i].style.backgroundColor = "red";
+
+        let speed = document.getElementById("speed").value;
+        await new Promise(resolve => setTimeout(resolve, speed));
+
+       if (parseInt(bars[i].innerText) === target) {
+
+    bars[i].style.backgroundColor = "green";
+    document.getElementById("status").innerText = "Element Found";
+
+    // Re-enable controls
+    document.getElementById("generate").disabled = false;
+    document.getElementById("sort").disabled = false;
+    document.getElementById("search").disabled = false;
+    document.getElementById("size").disabled = false;
+    document.getElementById("speed").disabled = false;
+
+    return;
+ }
+    }
+
+    document.getElementById("status").innerText = "Element Not Found";
+    document.getElementById("generate").disabled = false;
+    document.getElementById("sort").disabled = false;
+    document.getElementById("search").disabled = false;
+    document.getElementById("size").disabled = false;
+    document.getElementById("speed").disabled = false;  
+}
+
+function toggleTheme(){
+    document.body.classList.contains("dark-mode");
+    let button = document.getElementById("theme");
+    if(document.body.classList.contains("dark-mode")){
+        button.innerText ="☀️Light Mode";
+        localStorage.setltem("theme","dark");
+    }
+    else{
+        button.innerText ="🌙Dark mode";
+        localStorage.setltem("theme","light");
+    }
+}
+
+window.onload = function (){
+    generateArray();
+    updateAlgorithmInfo;
+    if(localStorage.getItem("theme")==="dark"){
+        document.body.classList.add(dark-mode);
+        document.getEelement("theme").innerText="☀️Light Mode";
+    }
+};
